2c4a7f9fe2989a492663e022966ccb8064404794
Hertz <hz@hertz.zone>
2022-01-25 07:29:24 +0800

    我好菜啊

--- generator.sh
+++ generator.sh
@@ -1,12 +1,12 @@
 main=index.html
 cat /dev/null > $main
-echo $'<!DOCTYPE html><html lang="zh-CN"><head><meta charset="UTF-8"><title>Hertz\'s Blog</title></head><body><h1>Hertz\'s Blog</h1><p>Seriously, it is really a blog in 2021!</br><a href="generator.sh">生成器文件</a> 作者不可考</p><ul>' > $main
+echo $'<!DOCTYPE html><html lang="zh-CN"><head><meta charset="UTF-8"><title>Hertz\'s Blog</title></head><body><h1>Hertz\'s Blog</h1><p>Seriously, it is really a blog in 2021!<br><a href="generator.sh">生成器文件</a> 作者不可考</p><ul>' > $main
 for i in `ls`; do
   if [ $i != 'index.html' ] && [ $i != 'generator.sh' ] && [ $i != 'main.html' ]; then
     echo '<li><a target="_blank" href="'$i'">'$i'</a></li>' >> $main
   fi
 done
-echo '</ul>Github：<a target="_blank" href="https://github.com/HertzYang/blog">HertzYang/blog</a></br>' >> $main
+echo '</ul>Github：<a target="_blank" href="https://github.com/HertzYang/blog">HertzYang/blog</a><br>' >> $main
 echo '<ul>友情链接：' >> $main
 echo '<li><a target="_blank" href="https://www.6700.cc">Sun Blog</a></li>' >> $main
 echo '<li><a target="_blank" href="https://zhiccc.net">Air Home</a></li>' >> $main
